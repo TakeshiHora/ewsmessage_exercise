25a4a9c1b24b8a1bb096fb7591376a50d1127501
TakeshiHora <a1936th@aiit.ac.jp>
2020-06-06 00:14:56 +0900

    added DisasterClass

--- src/main/java/CallConversionMT43.java
+++ src/main/java/CallConversionMT43.java
@@ -5,7 +5,11 @@ public class CallConversionMT43 {
     public static void main(String[] args) {
         //引数は実際は入力値とする。入力方法は別途検討する
         RadixConversion mt43Code = new RadixConversion("9AADF2C");
-    
+        
+        //災害区分の表示
+        DisasterType disasterType = new DisasterType(mt43Code.hexadecimalToBinaryconversion());
+        System.out.println(disasterType.reportCategoryConversion());
+        
         //通報区分が「震度」の場合に呼び出す処理
         EarthquakeEarlyWarning earthquakeEarlyWarning = new EarthquakeEarlyWarning(mt43Code.hexCode);
         
